ffc051523a4cb4afb0a18003a7aa97a0a86ab86d
ManishR17 <manishreddy17182gmail.com>
2020-05-10 12:37:08 +0530

    programs

---
+++ src/Lists/HashMapDemo.java
@@ -0,0 +1,23 @@
+package Lists;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class HashMapDemo {
+    public static void main(String[] args)
+    {
+        Map<Integer,String> mp=new HashMap<Integer,String>();
+        {
+            mp.put(1, "harley");
+            mp.put(2, "ktm");
+            mp.put(3, "jawa");
+            System.out.println(mp);
+
+
+
+        }
+    }
+
+
+}
